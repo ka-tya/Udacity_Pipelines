87e59b4ddf67290e9ac99d8fe87c92e03881f42e
ka-tya <katya.huster@gmail.com>
2020-04-08 02:22:20 -0400

    Update train_classifier.py
    prev reviewer said it was not working. not sure what the review meant by error - works just fine for me. completes and the output goes into run.py and executes with no errors. extremely slow though
    I changed n_jobs to 1 from -1 . I do not see performance improvement whatsoever

--- workspace/models/train_classifier.py
+++ workspace/models/train_classifier.py
@@ -10,9 +10,7 @@ from nltk.corpus import stopwords as sw
 from nltk import WordNetLemmatizer
 from nltk import sent_tokenize
 from nltk import pos_tag
-nltk.download('punkt')
-nltk.download('averaged_perceptron_tagger')
-nltk.download('stopwords')
+nltk.download(['punkt', 'wordnet','averaged_perceptron_tagger', 'stopwords'])
 from sklearn.pipeline import Pipeline
 from sklearn.multioutput import MultiOutputClassifier
 from sklearn.neighbors import KNeighborsClassifier
@@ -22,7 +20,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import classification_report
 import pickle
-
+# load messages dataset
 def load_data(database_filepath, table_name='PreppedDataTable'):
     """Load cleaned data from database into dataframe.
     Args:
@@ -41,6 +39,54 @@ def load_data(database_filepath, table_name='PreppedDataTable'):
 
     return X, y, category_names
 
+
+# ### 3. Split `categories` into separate category columns.
+# - Split the values in the `categories` column on the `;` character so that each value becomes a separate column. 
+# - Use the first row of categories dataframe to create column names for the categories data.
+# - Rename columns of `categories` with new column names.
+
+# create a dataframe of the 36 individual category columns
+def clean_data(df):
+    """Clean data.
+    Args:
+        df: pandas.DataFrame
+    Return:
+        pandad.DataFrame
+    """
+    # create a dataframe of the 36 individual category columns
+    categories = df['categories'].str.split(';', expand=True)
+
+    # select the first row of the categories dataframe
+    row = categories.loc[0]
+
+    # use this row to extract a list of new column names for categories.
+    # one way is to apply a lambda function that takes everything 
+    # up to the second to last character of each string with slicing
+    category_colnames = row.str[:-2].str.strip().tolist()
+    # rename the columns of `categories`
+    categories.columns = category_colnames
+
+
+    ### Convert category values to just numbers 0 or 1
+    for column in categories:
+        print(column)
+    # set each value to be the last character of the string
+        categories[column] = categories[column].str[-1:]
+    # convert column from string to numeric
+        categories[column] = categories[column].astype('int') 
+    # Convert all value into binary (0 or 1)
+        categories[column] = categories[column].astype(int)
+    # ### 5. Replace `categories` column in `df` with new category columns. 
+    # drop the original categories column from `df`
+    df = df.drop(columns = 'categories')
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat([df, categories], axis =1)
+    ### 6. Remove duplicates.
+    # drop duplicates
+    df = df.drop_duplicates()
+    
+    return df
+
 def tokenize(text, lemmatizer = WordNetLemmatizer()):
         """
         Returns a normalized, lemmatized list of tokens from a document by
@@ -49,6 +95,13 @@ def tokenize(text, lemmatizer = WordNetLemmatizer()):
         speech tags to look up the lemma in WordNet, and returns the lowercase
         version of all the words, removing stopwords and punctuation.
         """
+        # find URLs
+        url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+        found_urls = re.findall(url_regex, text)
+        for url in found_urls:
+            text = text.replace(url, 'urlplaceholder')
+            
+           
         tokens = []
         # Break the document into sentences
         for sent in sent_tokenize(text):
@@ -80,10 +133,10 @@ def build_model():
             ('classifier', MultiOutputClassifier(KNeighborsClassifier(n_neighbors=3))),
         ])
     parameters = {
-     'classifier__estimator__n_neighbors':list(range(3,15)), #I tested with 3-6
+     'classifier__estimator__n_neighbors':list(range(3,10)), #had to reduce the range due to extremely slow execution of the model 
      'classifier__estimator__weights':['uniform', 'distance']}
 
-    cv = GridSearchCV(pipeline, param_grid=parameters, cv=2, n_jobs=-1, verbose=3)
+    cv = GridSearchCV(pipeline, param_grid=parameters, cv=2, n_jobs=1, verbose=3)
     return cv
 
 def evaluate_model(model, X_test, Y_test,category_names):
@@ -109,13 +162,13 @@ def save_model(model, model_filepath):
     """
     with open(model_filepath, 'wb') as file:
         pickle.dump(model, file)
-                
+
 def main():
     if len(sys.argv) == 3:
         database_filepath, model_filepath = sys.argv[1:]
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
         X, Y, category_names = load_data(database_filepath)
-        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.8) #even with 50/50 runs over 5 hrs
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3) #even with 50/50 runs over 5 hrs
         
         print('Building model...')
         model = build_model()
