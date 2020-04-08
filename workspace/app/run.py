dd4ec0b033ed496c79049885d0b1c0643a23eff9
ka-tya <katya.huster@gmail.com>
2020-04-08 02:17:58 -0400

    Update run.py
    updated to show the 2 charts

--- workspace/app/run.py
+++ workspace/app/run.py
@@ -1,10 +1,31 @@
 import json
 import plotly
+
+import sys
+import os
 import pandas as pd
-
-from nltk.stem import WordNetLemmatizer
+import numpy as np
+import string
+from sqlalchemy import create_engine
 from nltk.tokenize import word_tokenize
+import nltk
+from nltk.corpus import stopwords as sw
+from nltk import WordNetLemmatizer
+from nltk import sent_tokenize
 
+
+from nltk import pos_tag
+nltk.download(['punkt', 'wordnet','averaged_perceptron_tagger', 'stopwords'])
+from sklearn.pipeline import Pipeline
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import classification_report
+import pickle
+import re
 from flask import Flask
 from flask import render_template, request, jsonify
 from plotly.graph_objs import Bar
@@ -14,62 +35,114 @@ from sqlalchemy import create_engine
 
 app = Flask(__name__)
 
-def tokenize(text):
-    tokens = word_tokenize(text)
-    lemmatizer = WordNetLemmatizer()
 
-    clean_tokens = []
-    for tok in tokens:
-        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
-        clean_tokens.append(clean_tok)
+def tokenize(text, lemmatizer = WordNetLemmatizer()):
+        """
+        Returns a normalized, lemmatized list of tokens from a document by
+        applying segmentation (breaking into sentences), then word/punctuation
+        tokenization, and finally part of speech tagging. It uses the part of
+        speech tags to look up the lemma in WordNet, and returns the lowercase
+        version of all the words, removing stopwords and punctuation.
+        """
+        # find URLs
+        url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+        found_urls = re.findall(url_regex, text)
+        for url in found_urls:
+            text = text.replace(url, 'urlplaceholder')
+    
+        tokens = []
+        # Break the document into sentences
+        for sent in sent_tokenize(text):
+            # Break the sentence into part of speech tagged tokens
+            for token, tag in pos_tag(word_tokenize(sent)):
+                # Apply preprocessing to the token
+                token = token.lower() 
+                token = token.strip()
+                token = token.strip('_')
+                token = token.strip('*')
 
-    return clean_tokens
+                # If punctuation or stopword, ignore token and continue
+                if token in set(sw.words('english')) or all(char in string.punctuation for char in token):
+                    continue
+                
+                tokens.append(token)
+        # Lemmatize the token and yield
+        tokens = [lemmatizer.lemmatize(t) for t in tokens]
+        return tokens
+
 
 # load data
-engine = create_engine('sqlite:///data/DisasterResponse.db')
+engine = create_engine('sqlite:///data/DisasterResponse.db')  
 df = pd.read_sql_table('PreppedDataTable', engine)
 
 
 # load model
 model = joblib.load("../workspace/models/classifier.pkl")
 
-
-#index webpage displays cool visuals and receives user input text for mode# @app.route(')
-@app.route('/inx')
+# index webpage displays cool visuals and receives user input text for model
+@app.route('/')
+@app.route('/index')
 def index():
     
-    # extract data needed fovisuals
-    # TODO: Below is an example - modify to extract data for your n visuals
-    genre_counts = df.groupby('genre').message.count()
-    genre_names = list(genre_counts.index)   
+    # extract data needed for visuals
+    # TODO: Below is an example - modify to extract data for your own visuals
+    # extract data needed for visuals
+    #plot 1
+    classes = df.columns[4:] 
+    class_counts = df[classes].sum().sort_values(ascending = False)
+    labels  = class_counts.index
+    #plot 2
+    #most common words for the social genre
+    word_list = []
+    for message in df[df.genre == 'social'].message:
+        word_list.append(tokenize(message))
+    word_counts = pd.Series(word_list).apply(pd.Series).stack().value_counts().head(10)
+    word_labels = word_counts.index
     #create visuals
     # TODO: Below is an example - modify to crea your own visls
     graphs = [
          { 'data': [
                 Bar(
-                  x=genre_names,
-                  y =genre_counts
+                  x=labels,
+                  y =class_counts
               )
           ],
 
             'layout': {
-                'title': 'Distribution of Messe Genres',
+                'title': 'Distribution of Message Classes',
                 'yaxis': {
                   'title': "Count"
               },
                  'xaxis': {
-                 'title': "Genre"
+                 'title': "Class"
               }
             }
-      }
+      },
+        {
+            'data': [
+                Bar(
+                    x=word_labels,
+                    y=word_counts
+                )
+            ],
+            'layout': {
+                'title': 'Top 10 Most common words in Social Genre',
+                'yaxis': {
+                    'title': "Counts"
+                },
+                'xaxis': {
+                    'title': "Word"
+                }
+            }
+        }
     ]
     
     # encode plotly graphs in JSON
     ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
-    graphJS= json.dumps(graphs, cls=plotly.utils.PtlyJSONEncoder)
+    graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
     
     # render web page with plotly graphs
-    return render_template('master.html', ids=ids, graphJSON=graphJS)
+    return render_template('master.html', ids=ids, graphJSON=graphJSON)
 
 
 # web page that handles user query and displays model results
@@ -89,9 +162,11 @@ def go():
         classification_result=classification_results
     )
 
+
 def main():
     app.run(host='0.0.0.0', port=3001, debug=True)
 
 
 if __name__ == '__main__':
     main()
+   
